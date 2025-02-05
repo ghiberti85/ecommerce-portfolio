7254f9377e5d3dcc8a311a0bd34dc14705e1db1b
ghiberti85 <ghiberti85@gmail.com>
2025-02-04 23:33:47 -0300

    feat: Header refactoring

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,16 @@
-// src/components/layout/Header.tsx
 'use client';
 
 import Link from 'next/link';
 import { useCategories } from '../../hooks/useProducts';
+import { useCart } from '../../context/CartContext';
 import { ShoppingCart } from 'lucide-react';
 
 export const Header = () => {
   const categories = useCategories();
+  const { cart } = useCart();
+
+  // Calcular a quantidade total de itens no carrinho
+  const totalItems = cart.reduce((total, item) => total + item.quantity, 0);
 
   return (
     <header className="bg-white shadow-md">
@@ -25,9 +29,16 @@ export const Header = () => {
             ))}
           </ul>
         </nav>
-        <Link href="/cart" className="text-gray-600 hover:text-gray-800">
-          <ShoppingCart size={24} />
-        </Link>
+        <div className="relative">
+          <Link href="/cart" className="text-gray-600 hover:text-gray-800 relative">
+            <ShoppingCart size={24} />
+            {totalItems > 0 && (
+              <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
+                {totalItems}
+              </span>
+            )}
+          </Link>
+        </div>
       </div>
     </header>
   );
