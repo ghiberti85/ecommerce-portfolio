cc0215ab55520655e2732cbaa7bbca33f3276f0f
ghiberti85 <ghiberti85@gmail.com>
2025-02-09 00:47:09 -0300

    fix: UI adjustments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,12 +30,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
         <CartProvider>
           <Header />
           <Toaster position="top-right" reverseOrder={false} />
-          {children}
+          <main className="flex-grow">{children}</main>
           <Footer />
         </CartProvider>
       </body>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,28 +6,25 @@ import { useCart } from '../../context/CartContext';
 
 export const Header = () => {
   const { cart } = useCart();
-
-
-
   const totalItems = cart.reduce((total, item) => total + item.quantity, 0);
 
   return (
-    <header className="bg-white shadow-md pt-6 pb-4">
-      <div className="container mx-auto px-4 py-4 flex flex-col sm:flex-row items-center justify-between">
-        <Link href="/" className="text-2xl font-bold text-gray-800">
+    <header className="bg-gray-900 text-white py-4">
+      <div className="container mx-auto px-4 flex justify-between items-center">
+        {/* Logo/Título */}
+        <Link href="/" className="text-2xl font-bold">
           Meu E-commerce
         </Link>
 
-        <div className="relative mt-2 sm:mt-0">
-          <Link href="/cart" className="text-gray-600 hover:text-gray-800 relative">
-            <ShoppingCart size={24} />
-            {totalItems > 0 && (
-              <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
-                {totalItems}
-              </span>
-            )}
-          </Link>
-        </div>
+        {/* Ícone do Carrinho */}
+        <Link href="/cart" className="relative">
+          <ShoppingCart size={28} />
+          {totalItems > 0 && (
+            <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
+              {totalItems}
+            </span>
+          )}
+        </Link>
       </div>
     </header>
   );
